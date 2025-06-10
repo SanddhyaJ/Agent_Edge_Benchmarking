674ea704494aa69c708642757311a2e8e4bbed1f
Sanddhya Jayabalan <sanddhyajayabalan@Sanddhyas-MacBook-Air.local>
2025-06-10 14:08:10 +0200

    added benchmark scripts for running all eval optim benchmarks

---
+++ code/bash_scripts/eval_optim_no_memory_METACOGNITION.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=METACOGNITION_evaloptim_nomem_GPT4o
+#SBATCH --partition=gpu,cpu
+#SBATCH --time=12:00:00   # Set the maximum run time (e.g. 60 minutes), after this your process will be killed, maximum 7 days
+#SBATCH --mem=32G         # How much RAM do you need?
+#SBATCH --cpus-per-task 12 # Number of CPU cores (I think this is the number of ht-threads, so you can select number of physical cores * 2)
+#SBATCH --mail-type=all # to receive E-Mail notifications
+#SBATCH --mail-user=sanddhya.jayabalan@tu-dresden.de # Set E-Mail address to receive notifications
+
+
+for f in $(ls ../../benchmarks/metacognition/*); do
+    echo "Running benchmark for dataset: $f"
+    name="$(basename "$file" .json)"
+    python ../eval_optimizer.py gpt-4o-2024-08-06 $name 1.0 evaloptimizer_no_memory ../../out
+done
